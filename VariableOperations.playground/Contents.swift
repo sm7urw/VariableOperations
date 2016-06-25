767e5d2cb2b10919b5060e3b100cf8caa1c71656
sm7urw <henrik@korslind.com>
2016-06-25 23:16:25 +0200

    Trying out variables

---
+++ VariableOperations.playground/Contents.swift
@@ -0,0 +1,31 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var variableOne = 15
+
+var variableTwo = 5
+
+var variableThree = variableOne + variableTwo
+
+var variableFour = variableTwo * 9
+
+var variableFive = variableOne - variableTwo
+
+var variableSix = variableOne / variableTwo
+
+var variableSeven = 10, variableEight = 20, variableNine = 30
+
+let variableTen = 40, variableEleven = 50, variableTwelve = 60
+
+var Thirteen: Double
+
+var Fourteen:Int = 423
+
+var Fifteen = "Hello"
+
+var Sixteen = "World!"
+
+var Seventeen = Fifteen + " " + Sixteen
+
+Seventeen = "\(Fifteen) \(Sixteen)"
